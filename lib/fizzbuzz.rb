20ee3c59976029016bafb6f42d9ec5d86650c73e
Dan Rosenthal <dan@heysparkbox.com>
2015-02-12 14:27:02 -0500

    test: pass fizz tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -9,12 +9,21 @@ class Game
   end
   
   def map_numbers(numbers)
-    @numbers_mapped = numbers
+    f = "fizz"
+    b = "buzz"
+    fb = "fizzbuzz"
+    @numbers_mapped = numbers.map do |x|
+      if x % 3 == 0
+        f
+      else
+        x
+      end
+    end
     output(numbers_mapped)
   end
   
   def output(numbers_mapped)
-    print numbers_mapped
+    #print numbers_mapped
   end
   
 end
