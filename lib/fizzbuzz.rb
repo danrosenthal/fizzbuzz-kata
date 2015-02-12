f0dd75dc8e98ce6b13ebb6858f58ea49c61db72b
Dan Rosenthal <dan@heysparkbox.com>
2015-02-12 15:20:54 -0500

    refactor: clean things up a bit

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,52 +1,51 @@
 class Game
-  attr_accessor :numbers, :numbers_mapped
+  attr_accessor :numbers_mapped
                 
   def initialize(start = 1, finish = 100)
     start = start
     finish = finish
-    @numbers = *(start..finish)
+    numbers = *(start..finish)
     map_numbers(numbers)
   end
   
   def map_numbers(numbers)
-    f = "fizz"
-    b = "buzz"
-    fb = "fizzbuzz"
-    bo = "boom"
-    @numbers_mapped = numbers.map do |x|
-      if divisible_by_3(x) && !divisible_by_5(x) && !divisible_by_12(x)
+    f = "fizz"; b = "buzz"; fb = "fizzbuzz"; bo = "boom"
+    @numbers_mapped = numbers.map do |number|
+      if divisible_by_3(number) && !divisible_by_5(number) && !divisible_by_12(number)
         f
-      elsif divisible_by_5(x) && !divisible_by_3(x) && !divisible_by_12(x)
+      elsif divisible_by_5(number) && !divisible_by_3(number) && !divisible_by_12(number)
         b
-      elsif divisible_by_3(x) && divisible_by_5(x) && !divisible_by_12(x)
+      elsif divisible_by_3_and_5(number) && !divisible_by_12(number)
         fb
-      elsif divisible_by_12(x)
+      elsif divisible_by_12(number)
         bo
       else
-        x
+        number
       end
     end
     output(numbers_mapped)
   end
   
   def output(numbers_mapped)
-    print numbers_mapped
+    puts numbers_mapped
   end
   
   private
   
-  def divisible_by_3(x)
-    x % 3 == 0
+  def divisible_by_3(number)
+    number % 3 == 0
   end
   
-  def divisible_by_5(x)
-    x % 5 == 0
+  def divisible_by_5(number)
+    number % 5 == 0
   end
   
-  def divisible_by_12(x)
-    x % 12 == 0
+  def divisible_by_3_and_5(number)
+    divisible_by_3(number) && divisible_by_5(number)
+  end
+  
+  def divisible_by_12(number)
+    number % 12 == 0
   end
   
 end
-
-Game.new
--- spec/lib/fizzbuzz_spec.rb
+++ spec/lib/fizzbuzz_spec.rb
@@ -2,20 +2,9 @@ require 'spec_helper'
 require 'fizzbuzz'
 
 describe Game do
-  context "when initiated without arguments creates an array of numbers" do
-    it "from 1 to 100" do
-      expect(subject.numbers.length).to eq(100)
-    end
-  end
-  
-  context "when initiated with arguments creates an array of numbers" do
-    it "starting with argument_1" do
-      game = Game.new(20,80)
-      expect(game.numbers.min).to eq(20)
-    end
-    it "ending with argument_2" do
-      game = Game.new(20,80)
-      expect(game.numbers.max).to eq(80)
+  context "when initiated without arguments" do
+    it "makes an array from 1 to 100" do
+      expect(subject.numbers_mapped.length).to eq(100)
     end
   end
   
@@ -40,18 +29,6 @@ describe Game do
   context "when we boom numbers" do
     it "replaces 12 with boom" do
       expect(subject.numbers_mapped[11]).to eq("boom")
-    end
-  end
-  
-  context "when we fizz, buzz, and fizzbuzz numbers" do
-    it "replaces 3 with fizz" do
-      expect(subject.numbers_mapped[2]).to eq("fizz")
-    end
-    it "replaces 5 with buzz" do
-      expect(subject.numbers_mapped[4]).to eq("buzz")
-    end
-    it "replaces 15 with fizzbuzz" do
-      expect(subject.numbers_mapped[14]).to eq("fizzbuzz")
     end
   end
   
