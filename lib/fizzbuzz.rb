6499f930e4ace8f2a528b9458142b1a4614c14af
Dan Rosenthal <dan@heysparkbox.com>
2015-02-12 14:15:27 -0500

    refactor test and add another case

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,34 +1,49 @@
 class Game
-  
-  attr_accessor :numbers, :numbers_fizzed, :numbers_buzzed, :numbers_fizzbuzzed, :numbers_fizzbuzzbuzzed
-  
+  attr_accessor :numbers, 
+                :numbers_fizzed, 
+                :numbers_buzzed, 
+                :numbers_fizzbuzzed, 
+                :numbers_fizzbuzzbuzzed,
+                :numbers_boomed
+                
   def initialize(start = 1, finish = 100)
     start = start
     finish = finish
     @numbers = *(start..finish)
-    fizz(numbers)
     fizzbuzz(numbers)
   end
   
   def fizzbuzz(numbers)
     fizzbuzz = "fizzbuzz"
     @numbers_fizzbuzzed = numbers.map { |x| (x % 3 == 0 && x % 5 == 0) ? fizzbuzz : x }
+    boom(numbers_fizzbuzzed)
   end
   
-  def fizz(numbers)
+  def boom(numbers_fizzbuzzed)
+    boom = "boom"
+    @numbers_boomed = numbers_fizzbuzzed.map { |x| x % 12 == 0 ? boom : x }
+    fizz(numbers_boomed)
+  end
+  
+  def fizz(numbers_boomed)
     fizz = "fizz"
-    @numbers_fizzed = numbers.map { |x| x % 3 == 0 ? fizz : x }
+    @numbers_fizzed = numbers_boomed.map { |x| x % 3 == 0 ? fizz : x }
     buzz(numbers_fizzed)
   end
   
   def buzz(numbers_fizzed)
     buzz = "buzz"
     @numbers_buzzed = numbers_fizzed.map { |x| x % 5 == 0 ? buzz : x }
-    #fizzbuzzbuzz(numbers_buzzed)
+    fizzbuzzbuzz(numbers_buzzed)
   end
   
-  def fizzbuzzbuzz
+  def fizzbuzzbuzz(numbers_buzzed)
     @numbers_fizzbuzzbuzzed = numbers_buzzed
+    output(numbers_fizzbuzzbuzzed)
+  end
+  
+  def output(numbers_fizzbuzzbuzzed)
+    print numbers_fizzbuzzbuzzed
   end
   
 end
