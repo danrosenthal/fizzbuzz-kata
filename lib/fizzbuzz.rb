ec4d381e3a7c31a469a0405d931dddea1c9a5acf
Dan Rosenthal <dan@heysparkbox.com>
2015-02-12 11:04:07 -0500

    test: passes fizz test and fails buzz test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,22 @@
 class Game
   
-  attr_accessor :stuff
+  attr_accessor :numbers, :numbers_fizzed, :numbers_buzzed
   
   def initialize(start = 1, finish = 100)
-    @start = start
-    @finish = finish
-    @stuff = *(start..finish)
+    start = start
+    finish = finish
+    @numbers = *(start..finish)
+    fizz(numbers)
+  end
+  
+  def fizz(numbers)
+    fizz = "fizz"
+    @numbers_fizzed = numbers.map { |x| x % 3 == 0 ? fizz : x }
+    buzz(numbers_fizzed)
+  end
+  
+  def buzz(numbers_fizzed)
+    @numbers_buzzed = numbers_fizzed
   end
   
 end
--- spec/lib/fizzbuzz_spec.rb
+++ spec/lib/fizzbuzz_spec.rb
@@ -2,22 +2,36 @@ require 'spec_helper'
 require 'fizzbuzz'
 
 describe Game do
-  context "when initiated Game creates an array of numbers" do
-    it "makes an array up to 100 without arguments" do
+  context "when initiated without arguments creates an array of numbers" do
+    it "from 1 to 100" do
       game = Game.new
-      expect(game.stuff.length).to be == 100
-    end
-    it "makes an array from argument 1 to argument 2 with arguments" do
-      game = Game.new(20,80)
-      expect(game.stuff.min).to be == 20
-      expect(game.stuff.max).to be == 80
+      expect(game.numbers.length).to eq(100)
     end
   end
   
-  context "when we fizz the stuff array" do
-    it "should have numbers divisible by three replaced with fizz" do
-      game = Game.new
-      expect(game.stuff[2]).to be == "fizz"
+  context "when initiated with arguments creates an array of numbers" do
+    it "starting with argument_1" do
+      game = Game.new(20,80)
+      expect(game.numbers.min).to eq(20)
+    end
+    it "ending with argument_2" do
+      game = Game.new(20,80)
+      expect(game.numbers.max).to eq(80)
     end
   end
+  
+  context "when we fizz numbers" do
+    it "replaces three with fizz" do
+      game = Game.new
+      expect(game.numbers_fizzed[2]).to eq("fizz")
+    end
+  end
+  
+  context "when we buzz numbers" do
+    it "replaces five with buzz" do
+      game = Game.new
+      expect(game.numbers_buzzed[4]).to eq("buzz")
+    end
+  end
+  
 end
